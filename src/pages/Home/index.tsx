ead304d58a0c806a38f38061ee703e8305039ed0
Luka Tassano <lukatassano070@gmail.com>
2020-06-07 03:07:31 -0300

    fix description Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,7 +15,7 @@ const Home: React.FC = () => {
         <main>
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>
-            Ajudamos pessoas a encontrarem pontos de coletade forma eficiente.
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
           </p>
 
           <Link to="/create-point">
